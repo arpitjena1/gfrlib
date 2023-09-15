613b3fa5f089f62f94acd9587a5ba6ac5b668b8a
arpitjena1 <arpitjenadublin@gmail.com>
2023-09-15 09:41:13 -0700

    yo

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -5,11 +5,17 @@ ASSET (path_txt);
 void autonomous() {
 	using namespace gfr;
 	using namespace gfr::chassis;
+
+	//pure pursuit
 	follow(path_txt,3000,10,true);
-	
+
+
+	//boomerang
 	move({{24,24,90}}, 127, gfr::ASYNC|gfr::THRU);
 	intake.move(127);
 	waitUntilFinished(2);
+
+	//ramsete
 	chainedmoveto({{Pose{10,10,10}},Pose(20,20,20)},100,gfr::THRU);
 
 	
